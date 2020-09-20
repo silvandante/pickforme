c287e178e8a91f2f08715ac389c679bc7856a860
Anny Walker <annyufrr@gmail.com>
2020-09-20 14:12:34 -0300

    Updates

---
+++ precache-manifest.7f0e7576e4a917c23808c2dd4d904649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2e0695a1a020b421e9c35a33bd2cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "859d7e4eb3154eddcadb",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "d4ab380aaa8a59b2bf30",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "859d7e4eb3154eddcadb",
+    "url": "/static/js/2.237c618c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.237c618c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ab380aaa8a59b2bf30",
+    "url": "/static/js/main.c48675ce.chunk.js"
+  },
+  {
+    "revision": "e5b6f8275b778bd97599",
+    "url": "/static/js/runtime-main.42059429.js"
+  }
+]);
